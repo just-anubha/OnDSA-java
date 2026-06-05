f2c137de06cf89ee40a25f15c3066eb99a79120f
Anubha Goyal <24155159@kiit.ac.in>
2026-06-05 13:57:24 +0530

    Day 2 - Add InsertionSort

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class InsertionSort {
+public static void main(String args[]){
+    int temp;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the size of array: ");
+    int n=sc.nextInt();
+    int[] arr=new int[n];
+    System.out.println("Enter the elements of array: ");
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    for(int i=0;i<n;i++){
+        int j=i;
+        while(j>0 && arr[j]<arr[j-1]){
+            temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+            j--;
+        }
+    }
+    System.out.print("Sorted array: ");
+    for(int i=0;i<n;i++){
+        System.out.print(arr[i]+" ");
+    }
+}
+}
